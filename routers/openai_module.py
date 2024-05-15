ce7db78947ce0ca5ec5e787bc6eeaba02855cf16
abhij <abhijit.davande@neurontechnologies.co.uk>
2024-05-15 12:39:51 +0100

    With UI and openai apis added

--- routers/openai_module.py
+++ routers/openai_module.py
@@ -5,12 +5,17 @@ from src.objects import tred_json, pdf_document
 from src.pdf_extraction import read_pdf
 
 
-def chat_with_gpt(pdf_text):
+def chat_with_gpt(pdf_text, seed):
     api_key = os.environ.get("OPENAI_API_KEY")
     client = OpenAI(api_key=api_key)
 
     # prompt = f"Q: {question}\nContext: {pdf_text}"
     # response = client.completions.create(model="gpt-3.5-turbo-instruct", prompt=prompt, max_tokens=150)
+
+    # To ensure that the OpenAI client.chat.completions.create method gives
+    # consistent answers for the same input data, you need to set the seed parameter in the request.The seed
+    # parameter specifies a random seed value that controls the randomization process used by the model.Providing the
+    # same seed value for the same input data ensures deterministic behavior, resulting in consistent responses.
     response = client.chat.completions.create(
         model="gpt-4",  # Model name, best output
 
@@ -37,16 +42,19 @@ def chat_with_gpt(pdf_text):
                            "Pre Mod or Pre-Mod or Pre Modification or Pre-Modification:"
                            "Post Mod or Post-Mod or Post Modification or Post-Modification:"
                            "Changes in quantity of clamps, nuts & other loose items:"
+                           "VB with suffix and prefix:"
                            "Part Numbers in comma separated format:",
             },
 
         ],
         max_tokens=700,  # Increase if you need a more detailed response
-        temperature=0.7  # Adjust for creativity in response; lower for more factual
+        temperature=0.7,  # Adjust for creativity in response; lower for more factual
+        seed=seed
     )
 
     return response.choices[0].message.content
     # response.choices[0].text.strip()
+    # return response.json()
 
 
 # Example usage:
@@ -88,23 +96,60 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 
+def string_to_integer_sum_alphabet(string):
+    string = string.upper()  # Convert string to uppercase for consistency
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    total = ''
+    for char in string:
+        # 9223372036854775807
+        if len(total) > 18:
+            continue
+        if char.isalpha():  # Check if the character is an alphabet
+            position = alphabet.index(char) + 1  # Get the position of the alphabet
+            total += str(position)
+        elif char.isdigit():
+            total += str(char)
+    return int(total)
+
+
 def get_gpt_extract(pdf_path):
     # pdf_text = extract_text_from_pdf(pdf_path)
     pdf_doc = read_pdf(pdf_path)
     pdf_text = pdf_doc.get_all_text_data()
     # print(f"pdf_text = {pdf_text}")
 
-    # print(f"pdf_text = {pdf_text}")
-    gpt_extract = chat_with_gpt(pdf_text)
-    # print(f"gpt_extract = {gpt_extract}")
+    # To ensure that the OpenAI client.chat.completions.create method gives
+    # consistent answers for the same input data, you need to set the seed parameter in the request.The seed
+    # parameter specifies a random seed value that controls the randomization process used by the model.Providing the
+    # same seed value for the same input data ensures deterministic behavior, resulting in consistent responses.
+    seed_val = string_to_integer_sum_alphabet(os.path.basename(pdf_path))
+    print(f"seed val is {seed_val} for file {os.path.basename(pdf_path)}")
+    gpt_extract = chat_with_gpt(pdf_text, seed_val)
+    print(f"gpt_extract = {gpt_extract}")
     gpt_extract_as_arr = gpt_extract.split('\n')
 
     output = tred_json()
     for each_line in gpt_extract_as_arr:
         in_arr = each_line.split(':')
-        key = in_arr[0].strip()
-        val = in_arr[1].strip()
-        output.add_data(key, val)
+        if len(in_arr) > 1:
+            key = in_arr[0].strip()
+            val = in_arr[1].strip()
+
+            if (key == 'Part Numbers in comma separated format' or key == 'VB Numbers' or
+                    key == 'Changes in quantity of clamps, nuts & other loose items'):
+                if key == 'Part Numbers in comma separated format':
+                    key = 'Affected CIs'
+                elif key == 'VB':
+                    key = 'Affected VBs'
+                elif key == 'Changes in quantity of clamps, nuts & other loose items':
+                    key = 'Quantity Changes'
+
+                split_val = val.split(',')
+                output.add_data(key, split_val)
+            else:
+                output.add_data(key, val)
+        else:
+            print(f"WARNING::Line {each_line} can not be split!")
 
     return output
 
